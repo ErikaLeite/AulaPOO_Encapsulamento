0573a004e5f0d2acafa04d67a1e7191397511d49
ERIKA LEITE PAIVA <you@example.com>
2020-12-09 13:43:42 -0300

    Estrutura de Encapsulamento pronta

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AulaPOO_Encapsulamento.classes;
+
+namespace AulaPOO_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MasterCard mc = new MasterCard();
+            
+        }
+    }
+}
---
+++ classes/MasterCard.cs
@@ -0,0 +1,16 @@
+namespace AulaPOO_Encapsulamento.classes
+{
+    public class MasterCard : Cartao
+    {
+        //ATRIBUTOS PRÓPRIOS DA SUBCLASSE MASTERCARD
+        private int parcelas;
+
+        //MÉTODOS PRÓRPIOS DA SUBCLASSE
+        public string ComprarComDesconto(){
+            return "";
+        }
+
+        
+
+    }
+}
